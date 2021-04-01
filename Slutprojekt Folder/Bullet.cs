bb7f62d7ac06c1537fd7b71007a2021a5c760d8c
Leo <leo.anderssonguillou@elev.ga.ntig.se>
2021-04-01 11:06:54 +0200

    First

---
+++ Slutprojekt Folder/Bullet.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Slutprojekt
+{
+    public class Bullet
+    {
+        public float x;
+        public float y;
+
+        public Bullet(float x_, float y_)
+        {
+            x = x_;
+            y = y_;
+        }
+    }
+}
---
+++ Slutprojekt Folder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
+
+namespace Slutprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1000, 800, "Slutprojekt");
+            Raylib.SetTargetFPS(60);
+
+
+            List<Bullet> bullets = new List<Bullet>();
+
+            bullets.Add(new Bullet(500, 780));
+            bullets.Add(new Bullet(600, 600));
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+
+                foreach (Bullet shot in bullets)
+                {
+                    Raylib.DrawCircle((int)shot.x, (int)shot.y, 20, Color.RED);
+                    shot.x -= 1;
+                    shot.y -= 2;
+                }
+
+
+
+                Raylib.EndDrawing();
+
+            }
+
+
+        }
+    }
+}
