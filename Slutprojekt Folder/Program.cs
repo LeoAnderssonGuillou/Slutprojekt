4a48e8a29f72d4659f65e8610f59066a8520c0ab
Leo <leo.anderssonguillou@elev.ga.ntig.se>
2021-04-15 11:05:03 +0200

    WalterSpray2

--- Slutprojekt Folder/Program.cs
+++ Slutprojekt Folder/Program.cs
@@ -11,32 +11,34 @@ namespace Slutprojekt
             Raylib.InitWindow(1000, 800, "Slutprojekt");
             Raylib.SetTargetFPS(60);
 
-
+            //List of bullets
             List<Bullet> bullets = new List<Bullet>();
             // bullets.Add(new Bullet(500, 780, 1, 1));
             // bullets.Add(new Bullet(600, 600, 1, 1));
 
+            //Aiming
             float directionX = 0;
             float directionY = 90;
 
             while (!Raylib.WindowShouldClose())
             {
+                //Setup
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
-
                 Texture2D walter = Raylib.LoadTexture("walter.png");
+                Texture2D walter2 = Raylib.LoadTexture("walter2.png");
 
-
-
+                //Draw and move bullets
                 foreach (Bullet shot in bullets)
                 {
-                    Raylib.DrawTexture(walter, (int)shot.x, (int)shot.y, Color.WHITE);
-                    //Raylib.DrawCircle((int)shot.x, (int)shot.y, 20, Color.RED);
+                    //Raylib.DrawTexture(walter, (int)shot.x, (int)shot.y, Color.WHITE);
+                    Raylib.DrawCircle((int)shot.x, (int)shot.y, 20, Color.RED);
                     shot.x += shot.xSpeed / 10;
                     shot.y -= shot.ySpeed / 10;
                 }
 
 
+                //Aim with arrow keys
                 if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) && directionX < 90)
                 {
                     directionX += 3;
@@ -47,9 +49,23 @@ namespace Slutprojekt
                 }
                 directionY = 90 - directionX * directionX / 90;
 
+
+                //Draw aiming indicator
+                int aimX = (int)(500 + directionX);
+                int aimY = (int)(600 - directionY);
+                Raylib.DrawCircle(aimX, aimY, 20, Color.BLACK);
+                Raylib.DrawTexture(walter, 455, 540, Color.WHITE);
+
+
+                // Raylib.DrawCircle((int)(500 + directionX / 2), (int)(600 - directionY / 2), 20, Color.BLACK);
+                // Raylib.DrawCircle((int)(500), (int)(600), 20, Color.BLACK);
+
+
+                //Shoot bullet
                 if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
                 {
-                    bullets.Add(new Bullet(500, 600, directionX, directionY));
+                    bullets.Add(new Bullet(aimX, aimY, directionX, directionY));
+                    Raylib.DrawTexture(walter2, 455, 540, Color.WHITE);
                 }
 
 
