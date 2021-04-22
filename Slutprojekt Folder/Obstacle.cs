3ad208797921acb27273cda96e0fadcd557a1249
LeoAnderssonGuillou <leo.anderssonguillou@elev.ga.ntig.se>
2021-04-22 10:48:58 +0200

    Stuff blink when damaged

--- Slutprojekt Folder/Obstacle.cs
+++ Slutprojekt Folder/Obstacle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Numerics;
+using Raylib_cs;
 
 namespace Slutprojekt
 {
@@ -9,12 +10,18 @@ namespace Slutprojekt
 
         public float xSpeed;
         public float ySpeed;
+        public int hp;
+        public Rectangle box;
+        public Color color = new Color(255, 255, 255, 255);
+        public int red = 255;
+        public Texture2D image = Raylib.LoadTexture("floppa.png");
 
-        public Obstacle(float x_, float y_, float xSpeed_, float ySpeed_)
+        public Obstacle(float x_, float y_, float xSpeed_, float ySpeed_, int hp_)
         {
             pos = new Vector2(x_, y_);
             xSpeed = xSpeed_;
             ySpeed = ySpeed_;
+            hp = hp_;
         }
     }
 }
--- Slutprojekt Folder/Program.cs
+++ Slutprojekt Folder/Program.cs
@@ -9,6 +9,7 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
+            //Setup
             Raylib.InitWindow(1000, 800, "Slutprojekt");
             Raylib.SetTargetFPS(60);
 
@@ -20,18 +21,23 @@ namespace Slutprojekt
             Vector2 direction = new Vector2(0, 0);
             float aimAngle = 0;
 
-            obstacles.Add(new Obstacle(500, 50, 1, 10));
+            //Cooldown variables
+            int shootCool = 0;
+
+            obstacles.Add(new Obstacle(0, 0, 1, 2, 10));
+            obstacles.Add(new Obstacle(500, 0, 0, 2, 10));
+            obstacles.Add(new Obstacle(1000, 0, -1, 2, 10));
 
             while (!Raylib.WindowShouldClose())
             {
-                //Setup
+                //Setup frame
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
                 Texture2D walter = Raylib.LoadTexture("walter.png");
                 Texture2D walter2 = Raylib.LoadTexture("walter2.png");
 
 
-                //Draw and move bullets
+                //Draw and move objetcs
                 HandleObjects(bullets, obstacles);
 
                 //Aim with arrow keys
@@ -44,7 +50,7 @@ namespace Slutprojekt
                 AimIndicator(direction, walter);
 
                 //Shoot bullet
-                ShootBullet(bullets, direction, walter2);
+                shootCool = ShootBullet(bullets, direction, walter2, shootCool);
 
 
                 Raylib.EndDrawing();
@@ -54,21 +60,54 @@ namespace Slutprojekt
 
         }
 
-        //Draw and move bullets
+
+
+        //Draw/move objects and handle collisions
         static void HandleObjects(List<Bullet> bulletList, List<Obstacle> obstacleList)
         {
+            //Draw/move bullets
             foreach (Bullet shot in bulletList)
                 {
-                    Raylib.DrawCircle((int)shot.pos.X, (int)shot.pos.Y, 15, Color.RED);
-                    shot.pos.X += shot.xSpeed / 10;
-                    shot.pos.Y -= shot.ySpeed / 10;
+                    Raylib.DrawCircle((int)shot.pos.X, (int)shot.pos.Y, 15, Color.GREEN);
+                    shot.pos.X += shot.xSpeed / 8;
+                    shot.pos.Y -= shot.ySpeed / 8;
                 }
             
-            foreach (Obstacle enemy in obstacleList)
+            //Draw/move obstacles and check all possible collisions
+            //To remove objects from these lists using loops, it is required to go through a for-loop in reverse
+            for (int i = obstacleList.Count - 1; i >= 0; i--)
                 {
-                    Raylib.DrawCircle((int)enemy.pos.X, (int)enemy.pos.Y, 15, Color.RED);
+                    Obstacle enemy = obstacleList[i];
+                    int size = 80;
+                    enemy.color = new Color(255, enemy.red, enemy.red, 255);
+                    enemy.box = new Rectangle(enemy.pos.X, enemy.pos.Y, size, size);
+                    Raylib.DrawTexture(enemy.image, (int)enemy.pos.X, (int)enemy.pos.Y, enemy.color);
                     enemy.pos.X += enemy.xSpeed;
                     enemy.pos.Y += enemy.ySpeed;
+
+                    //If collision is detected, remove bullet and damage obstacle.
+                    for (int x = bulletList.Count - 1; x >= 0; x--)
+                        {
+                            if (Raylib.CheckCollisionCircleRec(bulletList[x].pos, 15, enemy.box))
+                            {
+                                bulletList.RemoveAt(x);
+                                enemy.hp -= 1;
+                                enemy.red = 155;
+                            }
+                        }
+
+                    //Make obstacle less red, if red less than 0, make red 0
+                    enemy.red += 15;
+                    if (enemy.red > 255)
+                    {
+                        enemy.red = 255;
+                    }
+
+                    //Remove obstacle if its HP is 0
+                    if (enemy.hp < 1)
+                    {
+                        obstacleList.Remove(enemy);
+                    }
                 }
         }
 
@@ -104,15 +143,21 @@ namespace Slutprojekt
         }
 
         //Create new bullet and draw character shooting
-        static void ShootBullet(List<Bullet> bulletList, Vector2 direc, Texture2D character)
+        static int ShootBullet(List<Bullet> bulletList, Vector2 direc, Texture2D character, int cool)
         {
             if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
                 {
-                    int aimX = (int)(500 + direc.X);
-                    int aimY = (int)(750 - direc.Y);
-                    bulletList.Add(new Bullet(aimX, aimY, direc.X, direc.Y));
                     Raylib.DrawTexture(character, 455, 690, Color.WHITE);
+                    if (cool < 1)
+                    {
+                        int aimX = (int)(500 + direc.X);
+                        int aimY = (int)(750 - direc.Y);
+                        bulletList.Add(new Bullet(aimX, aimY, direc.X, direc.Y));
+                        cool = 4;
+                    }
                 }
+                cool--;
+                return cool;
         }
 
     }
